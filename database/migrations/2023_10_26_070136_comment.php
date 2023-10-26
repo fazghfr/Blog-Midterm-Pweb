390ebc39c8134503f0b9d84de82a031f5f14bec3
fauzan <fauzanalghifari963@gmail.com>
2023-10-26 14:07:02 +0700

    routes

--- database/migrations/2023_10_26_070136_comment.php
+++ database/migrations/2023_10_26_070136_comment.php
@@ -11,7 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('comments');
     }
 };
